34b95a03a0542238824a6d400dabd266e4625982
Vincent Goey <vincent.goey1003@gmail.com>
2020-02-16 02:24:44 +0800

    init project

---
+++ RecipeApp/RecipeAppTests/RecipeAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  RecipeAppTests.swift
+//  RecipeAppTests
+//
+//  Created by Kai Xuan on 16/02/2020.
+//  Copyright © 2020 Kai Xuan. All rights reserved.
+//
+
+import XCTest
+@testable import RecipeApp
+
+class RecipeAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
