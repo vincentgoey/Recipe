f5ad570692146c9ba4c16cfef9d7175f98f5760a
Vincent Goey <vincent.goey1003@gmail.com>
2020-02-16 02:29:55 +0800

    remove storyboard setting

---
+++ RecipeApp/RecipeApp/ViewController/HomeVC.swift
@@ -0,0 +1,18 @@
+//
+//  HomeVC.swift
+//  RecipeApp
+//
+//  Created by Kai Xuan on 16/02/2020.
+//  Copyright © 2020 Kai Xuan. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
